b8eefd9a32cfeef7b886df6f2106397278f50996
Augusto Boranga <guto.boranga@gmail.com>
2017-11-10 02:59:11 -0200

    create directory to results

--- test_functions/graphs/graph.R
+++ test_functions/graphs/graph.R
@@ -1,20 +1,22 @@
-data = read.table(file('shubert.txt'))
-plot(data, axes=FALSE, type="o", col="blue", pch='', xlab="Generation", ylab="Result")
+data = read.table(file('results/drop_wave_zoom.txt'))
+plot(data, axes=FALSE, type="o", col="blue", lwd=2, pch='', xlab="Generation", ylab="Result")
 
 iterations = data[[1]]
 results = data[[2]]
 
+first_result = results[1]
 last_result = results[length(results)]
+
 last_iteration = iterations[length(iterations)] + 1
 
-ticks_rate = abs(last_result/5)
+y_ticks_rate = abs((last_result - first_result)/5)
 
 # eixo X
 axis(1, at=10*0:last_iteration)
 
 # eixo Y
-axis(2, at=c(ticks_rate*0:last_result, last_result))
+axis(2, at=seq(first_result, last_result, by=-y_ticks_rate))
 
 box()
 
-title(main="Shubert: Evolution over generations")
+title(main="Drop Wave: Evolution over generations (zoom)")
